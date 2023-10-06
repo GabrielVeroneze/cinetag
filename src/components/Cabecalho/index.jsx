73e68cc6c1b4b0a155552d89b73fc2e9a0cd450a
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2023-10-06 00:01:30 -0300

    :sparkles: feat: Adiciona componente de cabeçalho

---
+++ src/components/Cabecalho/index.jsx
@@ -0,0 +1,24 @@
+import { Link } from "react-router-dom"
+import logo from './logo.png'
+import MenuLink from "components/MenuLink"
+import styles from './Cabecalho.module.scss'
+
+const Cabecalho = () => {
+    return (
+        <header className={styles.cabecalho}>
+            <Link to="/">
+                <img src={logo} alt="Logo do CineTag" />
+            </Link>
+            <nav className={styles.navegacao}>
+                <MenuLink url="/">
+                    Home
+                </MenuLink>
+                <MenuLink url="/Favoritos">
+                    Favoritos
+                </MenuLink>
+            </nav>
+        </header>
+    )
+}
+
+export default Cabecalho
