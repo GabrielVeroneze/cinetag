cff8983fe27cd60e68f130a5f02b2457352fb5c5
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2023-10-05 23:59:54 -0300

    :sparkles: feat: Importa o componente de rotas e utiliza ele

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './styles/global/reset.css'
-import './styles/global/estilosGlobais.scss'
+import 'styles/global/reset.css'
+import 'styles/global/estilosGlobais.scss'
+import AppRoutes from 'routes.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <h1>teste</h1>
+        <AppRoutes />
     </React.StrictMode>,
 )
