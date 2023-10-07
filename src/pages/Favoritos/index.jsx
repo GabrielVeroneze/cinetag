b3d6093d0e5093d33acc0da1cb09b8e2bead5a65
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2023-10-07 17:40:11 -0300

    :sparkles: feat: Finaliza página de Favoritos

--- src/pages/Favoritos/index.jsx
+++ src/pages/Favoritos/index.jsx
@@ -1,8 +1,20 @@
-import styles from "./Favoritos.module.scss"
+import Banner from "components/Banner"
+import Cards from "components/Cards"
+import CardsContainer from "components/CardsContainer"
+import Titulo from "components/Titulo"
 
 const Favoritos = () => {
     return (
-        <div>Favoritos</div>
+        <>
+            <Banner imagem="favoritos" />
+            <Titulo>
+                <h1>Meus Favoritos</h1>
+            </Titulo>
+            <CardsContainer>
+                <Cards id="1" titulo="O labirinto do Logcat" capa="https://caelum-online-public.s3.amazonaws.com/2802-react-praticando/img2.png" />
+                <Cards id="3" titulo="O poderoso JavaScript" capa="https://caelum-online-public.s3.amazonaws.com/2802-react-praticando/img4.png" />
+            </CardsContainer>
+        </>
     )
 }
 
