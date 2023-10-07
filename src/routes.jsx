21d08760a0bcc52e4f6213771e58c12f67818747
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2023-10-06 23:17:19 -0300

    :sparkles: feat: Adiciona "Layout Routes" e cria rota de favoritos

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,11 +1,16 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Inicio from "pages/Inicio"
+import Favoritos from "pages/Favoritos"
+import Layout from "components/Layout"
 
 const AppRoutes = () => {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<Inicio />} />
+                <Route element={<Layout />}>
+                    <Route path="/" element={<Inicio />} />
+                    <Route path="/favoritos" element={<Favoritos />} />
+                </Route>
             </Routes>
         </BrowserRouter>
     )
