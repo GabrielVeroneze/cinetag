cabc1c97bed527c3b3bde809180470028dc01f25
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2023-10-07 21:55:49 -0300

    :sparkles: feat: Fornece o contexto de Favoritos
    Fornece o contexto de Favoritos para as páginas de Inicio e Favoritos

--- src/routes.jsx
+++ src/routes.jsx
@@ -2,12 +2,19 @@ import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Inicio from "pages/Inicio"
 import Favoritos from "pages/Favoritos"
 import Layout from "components/Layout"
+import FavoritosProvider from "contexts/Favoritos"
 
 const AppRoutes = () => {
     return (
         <BrowserRouter>
             <Routes>
-                <Route element={<Layout />}>
+                <Route 
+                    element={
+                        <FavoritosProvider>
+                            <Layout />
+                        </FavoritosProvider>
+                    }
+                >
                     <Route path="/" element={<Inicio />} />
                     <Route path="/favoritos" element={<Favoritos />} />
                 </Route>
