e1b6d48a83f6357217455176c4728c992e7496ab
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2023-10-07 21:54:29 -0300

    :sparkles: feat: Cria contexto de Favoritos

---
+++ src/contexts/Favoritos.jsx
@@ -0,0 +1,16 @@
+import { createContext, useState } from "react";
+
+export const FavoritosContext = createContext()
+FavoritosContext.displayName = "Favoritos"
+
+export const FavoritosProvider = ({ children }) => {
+    const [favorito, setFavorito] = useState([])
+
+    return (
+        <FavoritosContext.Provider value={{ favorito, setFavorito }}>
+            {children}
+        </FavoritosContext.Provider>
+    )
+}
+
+export default FavoritosProvider
