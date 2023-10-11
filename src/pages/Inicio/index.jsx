28f57fa6d3074faf34caf3520d5d3dabbfe0a01a
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2023-10-10 22:52:33 -0300

    :sparkles: feat: Faz requisição para a API com os dados dos vídeos

--- src/pages/Inicio/index.jsx
+++ src/pages/Inicio/index.jsx
@@ -1,10 +1,25 @@
+import { useEffect, useState } from "react"
 import Banner from "components/Banner"
 import Cards from "components/Cards"
 import CardsContainer from "components/CardsContainer"
 import Titulo from "components/Titulo"
-import videos from "json/db.json"
 
 const Inicio = () => {
+    const [videos, setVideo] = useState([])
+
+    useEffect(() => {
+        async function buscarDadosVideos() {
+            try {
+                const response = await fetch('https://my-json-server.typicode.com/GabrielVeroneze/cinetag-api/videos')
+                const dados = await response.json()
+                setVideo(dados)
+            } catch (error) {
+                console.log(error);
+            }
+        }
+        buscarDadosVideos()
+    }, [])
+
     return (
         <>
             <Banner imagem="home" />
