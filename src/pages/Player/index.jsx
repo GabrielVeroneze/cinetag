904173747b1a8222c5140ae90d1ffd6641d855e9
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2023-10-10 23:34:21 -0300

    :sparkles: feat: Faz requisição com parâmetro para a API

--- src/pages/Player/index.jsx
+++ src/pages/Player/index.jsx
@@ -1,15 +1,27 @@
+import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
 import NaoEncontrada from "pages/NaoEncontrada"
 import Banner from "components/Banner"
 import Titulo from "components/Titulo"
-import videos from "json/db.json"
 import styles from "./Player.module.scss"
 
 const Player = () => {
     const { id } = useParams()
+    const [video, setVideo] = useState({})
 
-    const video = videos.find(video => video.id === Number(id))
-
+    useEffect(() => {
+        async function buscarDadosVideos() {
+            try {
+                const response = await fetch(`https://my-json-server.typicode.com/GabrielVeroneze/cinetag-api/videos?id=${id}`)
+                const dados = await response.json()
+                setVideo(...dados)
+            } catch (error) {
+                console.log(error);
+            }
+        }
+        buscarDadosVideos()
+    }, [id])
+    
     if (!video) {
         return <NaoEncontrada />
     }
